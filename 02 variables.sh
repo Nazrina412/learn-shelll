3ec3728a65924692f5a52587f10e974e5e9c329b
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 19:26:19 -0400

    learn variables

--- 02 variables.sh
+++ 02 variables.sh
@@ -14,3 +14,16 @@ echo ${name} Training - Faculty ${faculty}
 apple=20
 echo Apple price - ${apple}Dollars
 
+# Variable Substitutions
+# Command Substitution  - You can declare a variable by executing a command and store the ouptut in a variable
+# Syntax : var=$(command)
+
+# Arithmetic Substitution  - You can declare a variable by solving the given arithmetic expression
+# syntax: var=$((1+2-3/4))
+
+date=$(date)
+echo Today date is - $date
+
+add=$((2+3+4))
+echo 2+3+4 -$add
+
