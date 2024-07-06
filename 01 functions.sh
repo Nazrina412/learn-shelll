bfa2f0c6e04985e7c5af361c64c7f255734d1cd0
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 18:36:42 -0400

    payment scripting

---
+++ 01 functions.sh
@@ -0,0 +1,11 @@
+# function is a name given to a set of commands
+#syntax
+$ functions abc()
+#commands
+#comands
+#}
+
+how to call a functions simple call the function like a command
+SAMPLE() {
+  echo SAMPLE FUNCTIONS
+}
