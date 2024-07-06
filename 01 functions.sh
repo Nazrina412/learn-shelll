84cce35e87e69d625ace1cd8555a8f0031e8744d
Nazrina <nazrinakhan88@gmail.com>
2024-07-06 19:56:26 -0400

    learn variables

--- 01 functions.sh
+++ 01 functions.sh
@@ -1,11 +1,11 @@
 # function is a name given to a set of commands
 #syntax
-$ functions abc()
+##$ functions abc()
 #commands
 #comands
 #}
 
-how to call a functions simple call the function like a command
+##how to call a functions simple call the function like a command
 SAMPLE() {
   echo SAMPLE FUNCTIONS
 }
